cbc30bcb5d78ac04798d50635ef3849d5c7a28bf
Ryan Davis <ryand-ruby@zenspider.com>
2011-11-15 18:04:39 -0800

    nuked pkg and added to gitignore

--- pkg/otask-0.2/lib/otask.rb
+++
@@ -1,3 +0,0 @@
-class Otask
-  VERSION = "0.2.0"
-end
--- pkg/otask-0.2/test/test_otask.rb
+++
@@ -1,8 +0,0 @@
-require "minitest/autorun"
-require "otask"
-
-class TestOtask < MiniTest::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
-  end
-end
